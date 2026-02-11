80397c806c738cd1816690f893c7c2d8e77dee7d
PeddiPoojitha <poojithapeddi14@gmail.com>
2026-02-11 10:31:50 +0530

    switch statements

---
+++ JS_day2/index.js
@@ -0,0 +1,91 @@
+//1.Day of the Week Message
+
+// var days="Sunday";
+// switch(days){
+//     case "Monday":
+//         console.log("Start your week strong!");
+//         break;
+//     case "Tuesday":
+//         console.log("Keep going!");
+//         break;
+//     case "Wednesday":
+//         console.log("Halfway there!");
+//         break;
+//     case "Thursday":
+//         console.log("Almost the weekend!");
+//         break;
+//     case "Friday":
+//         console.log("Happy Friday!");
+//         break;
+//     case "Saturday":
+//     case "Sunday":
+//         console.log("Enjoy your weekend!");
+//         break;
+//     default:
+//         console.log("invalid Week");    
+// }
+
+
+//               2.Traffic Light Simulation
+
+// var singals="yellow"
+// switch(singals){
+//     case "red":
+//         console.log("Stop");
+//         break;
+//     case "yellow":
+//         console.log("Get Ready");
+//         break;
+//     case "green":
+//         console.log("Go")
+//         break;
+//     default:
+//         console.log("invalid single")
+        
+// }
+
+
+//            3.Discount Calculator
+
+// var amount=prompt("enter the purchase amount");
+// switch(true)
+// {
+//     case amount<0:
+//         console.log("invalid amount");
+//         break;
+//     case amount<50:
+//         console.log("No discount");
+//         break;
+//     case amount>=50 && amount<=100:
+//         console.log("5% discount");
+//         break;
+//     case amount>100 && amount<=200:
+//         console.log("10% discount");
+//         break;
+//     case amount>200:
+//         console.log("15% discount");
+//         break;
+//     default:
+//          console.log("invalid");       
+// }
+
+//               4.Restaurant Menu
+
+var dish=prompt("enter the items");
+switch(dish){
+    case "Biriyani":
+        console.log("Price: ₹180");
+        break;
+    case "shawarma":
+        console.log("Price: ₹120");
+        break;
+    case "fried rice":
+        console.log("Price: ₹100");
+        break;
+    case "veg pula":
+        console.log("Price: ₹90");
+        break;
+    default:
+        console.log("invalid item")
+        
+}
